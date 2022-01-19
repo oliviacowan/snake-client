601c464374fd18988643aad0b23dc1b1db238599
Olivia <olivia.cowan@hotmail.com>
2022-01-19 23:17:37 +0000

    fixed the exporting modules, created input.js and updated play.js

--- client.js
+++ client.js
@@ -1,11 +1,9 @@
 const net = require('net');
-const host = '165.227.47.243';
-const port = '50541';
 
 const connect = function () {
   const conn = net.createConnection({
-    host,
-    port
+    host: '165.227.47.243',
+    port: '50541'
   });
   conn.on('connect', () => {
     console.log('Connection established');
@@ -16,8 +14,6 @@ const connect = function () {
       conn.write('Move: up');
     }, 1000) 
   });
-
-
   
   conn.on('data', (data) => {
     console.log(`server says: ${data}`);
@@ -25,4 +21,6 @@ const connect = function () {
   conn.setEncoding('utf8');
   return conn;
 };
-module.exports = connect;
+module.exports = {
+  connect,
+};

2d4eafa82237d08f0a93566a49f7e812dd8c5b46
Olivia <olivia.cowan@hotmail.com>
2022-01-19 22:24:11 +0000

    played with the move commands

--- client.js
+++ client.js
@@ -12,7 +12,26 @@ const connect = function () {
   });
   conn.on('connect', () => {
     conn.write("Name: OC");
+    // setInterval(() => {
+    //   conn.write("Move: up");
+
+    // }, 50);
+    let moves = ["Move: up", "Move: left", "Move: down", "Move: down", "Move: right"];
+    let delay = 1000;
+    for (const move of moves) {
+    setTimeout(() => {
+      conn.write(move);
+    }, delay) 
+    delay += 50;
+  }
+    // conn.write("Move: up");
+    // conn.write("Move: down");
+    // conn.write("Move: left");
+    // conn.write("Move: right");
   });
+
+
+  
   conn.on('data', (data) => {
     console.log(`server says: ${data}`);
   });
--- play.js
+++ play.js
@@ -1,6 +1,7 @@
 const net = require('net');
 const connect = require('./client');
-//const {host, port} = require('./client');
+
+
 
 
 console.log("connecting...");
